3c27f30e74f4445137ea20fdabefb05a46791516
Brendan Ko <brenny0707@gmail.com>
2017-08-22 12:30:40 -0400

    Render page from Root

--- frontend/dineround.jsx
+++ frontend/dineround.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import {signup, login, logout} from './util/session_api_util.js'; //TESTING
 import configureStore from './store/store';
 //TESTING!!!
@@ -13,5 +14,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Welcome to DineRound!</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
