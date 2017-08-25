7903fe3a8bf9c2d06ebf5f40188886f3607154a6
Brendan Ko <brenny0707@gmail.com>
2017-08-25 17:32:42 -0400

    Add index method

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -2,7 +2,17 @@ class Api::ReservationsController < ApplicationController
 
   before_action :require_logged_in, only: [:create, :destroy, :update]
   def index
+    @reservations = Reservation.find_restaurant_reservations(
+    params[:restaurant_name],
+    params[:seats],
+    params[:date],
+    params[:time])
 
+    if @reservations.length = 0
+      render json: ["We're sorry, there are currently no tables available at the time you have chosen."]
+    else
+      render :index
+    end
   end
 
   def show
