63a5fd7cc59d04f64b98afaffc0be108e080c851
Brendan Ko <brenny0707@gmail.com>
2018-01-03 14:57:19 -0500

    Start changing unnecessary class components into functional components

--- frontend/components/restaurant/restaurant_detail_review_item.jsx
+++ frontend/components/restaurant/restaurant_detail_review_item.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-class RestaurantDetailReviewItem extends React.Component {
-
-  render() {
-    if (this.props.review === undefined ){
+const RestaurantDetailReviewItem = (props) => {
+    if (props.review === undefined ){
       return null;
     }
     let moment = require('moment');
-    let parseDate= moment.utc(this.props.review.created_at).format("LL");
-    let numfilledStars = this.props.review.overallRating;
+    let parseDate= moment.utc(props.review.created_at).format("LL");
+    let numfilledStars = props.review.overallRating;
     let numblankStars = 5 - numfilledStars;
     let stars = [];
     for (numfilledStars; numfilledStars > 0; numfilledStars--) {
@@ -23,21 +21,20 @@ class RestaurantDetailReviewItem extends React.Component {
         <div className="restaurant-review-content">
           <div className="restaurant-review-header">
             <div className="restaurant-review-header-overall-and-user">
-              <p className="review-overall-rating">{stars} Written by {this.props.review.userFName} on {parseDate}</p>
+              <p className="review-overall-rating">{stars} Written by {props.review.userFName} on {parseDate}</p>
             </div>
           </div>
-          <p className="restaurant-review-body">{this.props.review.body}</p>
+          <p className="restaurant-review-body">{props.review.body}</p>
         </div>
         <ul className="restaurant-review-ratings">
-          <li className="review-food-rating">FOOD {this.props.review.foodRating}</li>
-          <li className="review-ambiance-rating">AMBIANCE {this.props.review.ambianceRating}</li>
-          <li className="review-service-rating">SERVICE {this.props.review.serviceRating}</li>
-          <li className="review-value-rating">VALUE {this.props.review.valueRating}</li>
+          <li className="review-food-rating">FOOD {props.review.foodRating}</li>
+          <li className="review-ambiance-rating">AMBIANCE {props.review.ambianceRating}</li>
+          <li className="review-service-rating">SERVICE {props.review.serviceRating}</li>
+          <li className="review-value-rating">VALUE {props.review.valueRating}</li>
         </ul>
       </li>
     );
-  }
-}
+  };
 
 const mapStateToProps = (state, ownProps) => {
   return {
