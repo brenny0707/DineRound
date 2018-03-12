3ad8c421d370bcba95393e27809ddb0ae3c52abb
Brendan Ko <brenny0707@gmail.com>
2018-03-12 11:51:50 -0400

    Remove static suggestions list

--- frontend/components/search/search_restaurants_form.jsx
+++ frontend/components/search/search_restaurants_form.jsx
@@ -1,35 +1,5 @@
 import React from 'react';
 
-const suggestions = [
- { name: "15 East Restaurant"},
- { name: "ABC Kitchen"},
- { name: "Babbo Ristorante"},
- { name: "Blue Hill"},
- { name: "Gotham Bar and Grill"},
- { name: "Gramercy Tavern"},
- { name: "Jean-Georges"},
- { name: "Juniper"},
- { name: "Le Bernardin"},
- { name: "Marea"},
- { name: "Marta"},
- { name: "Minetta Tavern"},
- { name: "Momofuku Ko"},
- { name: "Morimoto New York"},
- { name: "Per Se"},
- { name: "Saint George"},
- { name: "Scarpetta"},
- { name: "Shake Shack - Grand Central Terminal"},
- { name: "Smith & Wollensky - New York"},
- { name: "The NoMad"}];
-
-// const getSuggestions = value => {
-//   const input = value.trim().toLowerCase();
-//   const inputLength = value.length;
-//
-//   return inputLength === 0 ? [] : suggestions.filter(suggestion => suggestion.name.toLowerCase().slice(0, inputLength) === input
-//   );
-// };
-
 class SearchRestaurantsForm extends React.Component {
   constructor(props) {
     super(props);
@@ -84,7 +54,7 @@ class SearchRestaurantsForm extends React.Component {
     else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
       let idx, prevSelected;
       let names = this.state["suggestions"].map( (suggestion) => {
-        return suggestion.name;
+        return suggestion;
       });
       if ($(".search-restaurants-suggestion.hovered-suggestion")[0]) {
         prevSelected = $(".search-restaurants-suggestion.hovered-suggestion")[0].innerText;
